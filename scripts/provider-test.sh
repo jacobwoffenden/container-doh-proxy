868807b4dd435c50f8cb838270c203f510d7bf3a
Jacob Woffenden <jacob@woffenden.io>
2022-02-18 13:18:08 +0000

    Added dnsutils to provider testing workflow Added extra config provider vars to provider testing workflow Added extra config provider to provider testing script only if in actions Remover PowerDNS provider

--- scripts/provider-test.sh
+++ scripts/provider-test.sh
@@ -1,13 +1,38 @@
 #!/usr/bin/env bash
 
+CLOUDFLARE_STATUS_EXPECTED_RESULT=$( dig cloudflarestatus.com +short )
+
 echo "Building Container"
-docker build --tag doh-proxy --file Containerfile . 
+dockerBuild=$( docker build --tag doh-proxy --file Containerfile . )
 
 # Testing providers that require no extra configuration
 for provider in $( jq -r 'keys[]' src/etc/doh-proxy/providers.json ); do
   echo "Testing Provider: ${provider}"
   echo "---> Starting Container [ ${provider} ]"
   dockerRun=$( docker run --detach --env PROVIDER=${provider} --name ${provider} doh-proxy )
+
+  sleep 2
+
+  echo "---> Sending Test Request"
+  sendTest=$( docker exec ${provider} dig +time=2 +tries=1 @127.0.0.1 -p 53 cloudflarestatus.com +short )
+  if [[ "${sendTest}" != "${CLOUDFLARE_STATUS_EXPECTED_RESULT}" ]]; then
+    echo "---> Test Failed"
+    dockerRm=$( docker rm --force ${provider} )
+    exit 1
+  else
+    echo "---> Test Passed"
+  fi
+  
+  echo "---> Deleting Container [ ${provider} ]"
+  dockerRm=$( docker rm --force ${provider} )
+done
+
+if [[ "${GITHUB_ACTIONS}" == "true" ]]; then
+  # Testing Cloudflare Zero Trust
+  export provider="cloudflare-zero-trust"
+  echo "Testing Provider: ${provider}"
+  echo "---> Starting Container [ ${provider} ]"
+  dockerRun=$( docker run --detach --env PROVIDER=${provider} --env CLOUDFLARE_ZERO_TRUST_ID=${CLOUDFLARE_ZERO_TRUST_ID} --name ${provider} doh-proxy )
 
   sleep 2
 
@@ -20,49 +45,28 @@ for provider in $( jq -r 'keys[]' src/etc/doh-proxy/providers.json ); do
   else
     echo "---> Test Passed"
   fi
-  
+
   echo "---> Deleting Container [ ${provider} ]"
   dockerRm=$( docker rm --force ${provider} )
-done
 
-# Testing Cloudflare Zero Trust
-export provider="cloudflare-zero-trust"
-echo "Testing Provider: ${provider}"
-echo "---> Starting Container [ ${provider} ]"
-dockerRun=$( docker run --detach --env PROVIDER=${provider} --env CLOUDFLARE_ZERO_TRUST_ID=${CLOUDFLARE_ZERO_TRUST_ID} --name ${provider} doh-proxy )
+  # Testing NextDNS
+  export provider="nextdns"
+  echo "Testing Provider: ${provider}"
+  echo "---> Starting Container [ ${provider} ]"
+  dockerRun=$( docker run --detach --env PROVIDER=${provider} --env NEXTDNS_ID=${NEXTDNS_ID} --name ${provider} doh-proxy )
 
-sleep 2
+  sleep 2
 
-echo "---> Sending Test Request"
-sendTest=$( docker exec ${provider} dig +time=2 +tries=1 @127.0.0.1 -p 53 cloudflarestatus.com +short || echo "TEST_FAILED" )
-if [[ "${sendTest}" == *"TEST_FAILED"* ]]; then
-  echo "---> Test Failed"
+  echo "---> Sending Test Request"
+  sendTest=$( docker exec ${provider} dig +time=2 +tries=1 @127.0.0.1 -p 53 cloudflarestatus.com +short || echo "TEST_FAILED" )
+  if [[ "${sendTest}" == *"TEST_FAILED"* ]]; then
+    echo "---> Test Failed"
+    dockerRm=$( docker rm --force ${provider} )
+    exit 1
+  else
+    echo "---> Test Passed"
+  fi
+
+  echo "---> Deleting Container [ ${provider} ]"
   dockerRm=$( docker rm --force ${provider} )
-  exit 1
-else
-  echo "---> Test Passed"
 fi
-
-echo "---> Deleting Container [ ${provider} ]"
-dockerRm=$( docker rm --force ${provider} )
-
-# Testing NextDNS
-export provider="nextdns"
-echo "Testing Provider: ${provider}"
-echo "---> Starting Container [ ${provider} ]"
-dockerRun=$( docker run --detach --env PROVIDER=${provider} --env NEXTDNS_ID=${NEXTDNS_ID} --name ${provider} doh-proxy )
-
-sleep 2
-
-echo "---> Sending Test Request"
-sendTest=$( docker exec ${provider} dig +time=2 +tries=1 @127.0.0.1 -p 53 cloudflarestatus.com +short || echo "TEST_FAILED" )
-if [[ "${sendTest}" == *"TEST_FAILED"* ]]; then
-  echo "---> Test Failed"
-  dockerRm=$( docker rm --force ${provider} )
-  exit 1
-else
-  echo "---> Test Passed"
-fi
-
-echo "---> Deleting Container [ ${provider} ]"
-dockerRm=$( docker rm --force ${provider} )
